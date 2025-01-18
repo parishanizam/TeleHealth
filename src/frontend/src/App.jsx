172f79387513f13e2f97f55656184f7c2a2bba08
PromishKandel <promishkandel@gmail.com>
2025-01-18 14:43:44 -0500

    Created frontend folder structure

---
+++ src/frontend/src/App.jsx
@@ -0,0 +1,9 @@
+// src/App.jsx
+import React from 'react'
+import AppRouter from './routes/index.jsx'
+
+function App() {
+  return <AppRouter />
+}
+
+export default App
---
+++ src/frontend/src/routes/index.jsx
@@ -0,0 +1,25 @@
+// Uncomment when the pages are created
+// import React from 'react'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import LandingPage from '../landing/LandingPage'
+// import ParentLogin from '../parents/pages/ParentLogin'
+// import ClinicianLogin from '../clinician/pages/ClinicianLogin'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <LandingPage />,
+//   },
+//   {
+//     path: '/parents/login',
+//     element: <ParentLogin />,
+//   },
+//   {
+//     path: '/clinicians/login',
+//     element: <ClinicianLogin />,
+//   },
+// ])
+
+// export default function AppRouter() {
+//   return <RouterProvider router={router} />
+// }
