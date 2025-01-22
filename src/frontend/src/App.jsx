669ace7d895c80690b68a978cd1c15b86438a352
mitcholll <mitchell.weingust@gmail.com>
2025-01-21 21:45:37 -0500

    Added AddClient pages 1 and 2 #333 Including button functionality to display Client Number

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,9 +1,13 @@
 // import AppRouter from './routes/index.jsx'
 // import ClientOverview from './clinicians/pages/ClientOverview.jsx'
-import ClinicianDashboard from './clinicians/pages/ClinicianDashboard.jsx'
+// import ClinicianDashboard from './clinicians/pages/ClinicianDashboard.jsx'
+// import ClinicianLoginPage from './clinicians/pages/ClinicianLoginPage.jsx'
+import AddClientPage from "./clinicians/pages/AddClientPage"
 
 function App() {
-  return <ClinicianDashboard />
+  return <AddClientPage />
+  // return <ClinicianLoginPage />
+  // return <ClinicianDashboard />
   // return <ClientOverview />
   // return <AppRouter />
 }
