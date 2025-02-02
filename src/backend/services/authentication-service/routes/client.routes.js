e320393eb44ad35e11afa2d3b6426c52616a1066
promish <promishkandel@gmail.com>
2025-02-02 11:55:11 -0500

    random push to main

--- src/backend/services/authentication-service/routes/client.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addClient } = require('../controllers/client.controller');
-
-router.post('/add-client', addClient);
-
-module.exports = router;
--- src/backend/services/authentication-service/routes/parent.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { parentSignup, parentLogin } = require('../controllers/parent.controller');
-
-router.post('/signup', parentSignup);
-// router.post('/login', parentLogin);
-
-module.exports = router;
