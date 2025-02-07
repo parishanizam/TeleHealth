f72f196356ae4da3d7741406a5786fbcc690b63e
mitcholll <mitchell.weingust@gmail.com>
2025-02-06 19:39:06 -0500

    Modified TestCompletePage to give the user the ability to go to test selection page to setup a new test, or go to the homepage. #413 Improved modularity by removing test setup functionality on TestCompletePage Modified NextButton to allow the ability to pass in different icons (or the option for none), with the default still being a right arrow

--- src/frontend/src/parents/components/NextButton.jsx
+++ src/frontend/src/parents/components/NextButton.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import NextArrow from "../../assets/nextarrow.svg";
 
-function NextButton({ to = "/", name = "Next" }) {
+function NextButton({ to = "/", name = "Next", icon = NextArrow }) {
   return (
     <Link to={to}>
       <div className="flex justify-center items-center px-4 py-2.5 bg-white rounded-lg border-blue-600 border-solid border-[1.5px] text-xl text-blue-600 cursor-pointer">
         <span>{name}</span>
-        <img
-          src={NextArrow}
-          alt="Next"
-          className="ml-2 w-5 h-5 object-contain"
-        />
+        {icon && (
+          <img
+            src={icon}
+            alt="Next"
+            className="ml-2 w-5 h-5 object-contain"
+          />
+        )}
       </div>
     </Link>
   );
