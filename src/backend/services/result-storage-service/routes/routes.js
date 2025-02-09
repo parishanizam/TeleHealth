f2b7ac64799393ef407d867f074046b786977881
Parisha Nizam <parisha.nizam@gmail.com>
2025-02-09 11:08:01 -0500

    Revert back result controller

--- src/backend/services/result-storage-service/routes/routes.js
+++ src/backend/services/result-storage-service/routes/routes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { submitAssessment, getAssessmentHistory, getAssessmentResults, calculateScores } = require("../controllers/results.controller");
+const { submitAssessment, getAssessmentHistory, getAssessmentResults } = require("../controllers/results.controller");
 
 // Submit a full assessment & update history
 router.post("/submit-assessment", submitAssessment);
 
-router.get("/resultstorage/calculate-scores/:parentUsername", calculateScores);
 // Get a parent's full assessment history
 router.get("/assessment-history/:username", getAssessmentHistory);
 
